24049024c27c984a38ffb6399d652932540868d7
Carl <carl@carlossus.com>
2016-06-08 23:40:46 +0800

    Add rate-limiting

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ const express = require('express');
 const points = require('./points');
 const backup_1 = require('./backup');
 const server = express();
+const limitCache = {};
 server.use(express.static('front'));
 server.get('/upvote/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
     points.upvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 server.get('/downvote/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
     points.downvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 server.get('/adduser/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
     points.addUser(req.params.name);
     res.json(points.getUsers());
 });
@@ -26,4 +39,28 @@ server.get('/ping', (req, res) => res.json({ ok: true }));
 server.listen(1716, () => console.log('Listening on port 1716'));
 setInterval(() => backup_1.default(), 60000);
 points.loadFromBackup();
+function ip(req) {
+    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+}
+function limit(address) {
+    // REQ_LIMIT requests per RESET_MS milliseconds
+    const REQ_LIMIT = 50;
+    const RESET_MS = 500;
+    if (limitCache[address] === undefined) {
+        limitCache[address] = 0;
+        setTimeout(() => delete limitCache[address], RESET_MS);
+    }
+    limitCache[address]++;
+    const canRequest = limitCache[address] < REQ_LIMIT;
+    console.log(limitCache[address], canRequest);
+    return canRequest;
+}
+function decrease(address) {
+    if (limitCache[address] === undefined)
+        return;
+    limitCache[address]--;
+    if (limitCache[address] > 0)
+        return;
+    delete limitCache[address];
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,36 @@ import backup from './backup';
 
 const server = express();
 
+const limitCache: { [address: string]: number } = {};
 
 server.use(express.static('front'));
 
 server.get('/upvote/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
+
     points.upvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 
 server.get('/downvote/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
+
     points.downvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 
 server.get('/adduser/:name', (req, res) => {
+    if (!limit(ip(req))) {
+        res.statusCode = 429;
+        return res.end();
+    }
+
     points.addUser(req.params.name);
     res.json(points.getUsers());
 });
@@ -37,3 +53,31 @@ server.listen(1716, () => console.log('Listening on port 1716'));
 setInterval(() => backup(), 60000);
 
 points.loadFromBackup();
+
+function ip(req: express.Request) {
+    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+}
+
+function limit(address: string): boolean {
+    // REQ_LIMIT requests per RESET_MS milliseconds
+    const REQ_LIMIT = 50;
+    const RESET_MS = 500;
+
+    if (limitCache[address] === undefined) {
+        limitCache[address] = 0;
+        setTimeout(() => delete limitCache[address], RESET_MS);
+    }
+
+    limitCache[address]++;
+    const canRequest = limitCache[address] < REQ_LIMIT;
+    console.log(limitCache[address], canRequest);
+
+    return canRequest;
+}
+
+function decrease(address: string): void {
+    if (limitCache[address] === undefined) return;
+    limitCache[address]--;
+    if (limitCache[address] > 0) return;
+    delete limitCache[address];
+}
