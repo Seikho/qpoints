3c956d924c8fd544da55f68fa35f478f7d0730e2
Carl <carl@carlossus.com>
2016-06-08 23:52:59 +0800

    Replace manual rate-limiting with express-brute

--- src/index.js
+++ src/index.js
@@ -2,30 +2,23 @@
 const express = require('express');
 const points = require('./points');
 const backup_1 = require('./backup');
+const Brute = require('express-brute');
 const server = express();
+const store = new Brute.MemoryStore();
+const brute = new Brute(store, {
+    freeRetries: 25,
+});
 const limitCache = {};
 server.use(express.static('front'));
-server.get('/upvote/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
+server.get('/upvote/:name', brute.prevent, (req, res) => {
     points.upvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
-server.get('/downvote/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
+server.get('/downvote/:name', brute.prevent, (req, res) => {
     points.downvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
-server.get('/adduser/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
+server.get('/adduser/:name', brute.prevent, (req, res) => {
     points.addUser(req.params.name);
     res.json(points.getUsers());
 });
@@ -39,27 +32,4 @@ server.get('/ping', (req, res) => res.json({ ok: true }));
 server.listen(1716, () => console.log('Listening on port 1716'));
 setInterval(() => backup_1.default(), 60000);
 points.loadFromBackup();
-function ip(req) {
-    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-}
-function limit(address) {
-    // REQ_LIMIT requests per RESET_MS milliseconds
-    const REQ_LIMIT = 50;
-    const RESET_MS = 1000;
-    if (limitCache[address] === undefined) {
-        limitCache[address] = 0;
-        setTimeout(() => delete limitCache[address], RESET_MS);
-    }
-    limitCache[address]++;
-    const canRequest = limitCache[address] < REQ_LIMIT;
-    return canRequest;
-}
-function decrease(address) {
-    if (limitCache[address] === undefined)
-        return;
-    limitCache[address]--;
-    if (limitCache[address] > 0)
-        return;
-    delete limitCache[address];
-}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,30 @@ import * as express from 'express';
 import * as points from './points';
 import backup from './backup';
 
+const Brute = require('express-brute');
 const server = express();
+
+const store = new Brute.MemoryStore();
+const brute = new Brute(store, {
+    freeRetries: 25, 
+});
 
 const limitCache: { [address: string]: number } = {};
 
 server.use(express.static('front'));
 
-server.get('/upvote/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
+server.get('/upvote/:name', brute.prevent, (req, res) => {
 
     points.upvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 
-server.get('/downvote/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
-
+server.get('/downvote/:name', brute.prevent, (req, res) => {
     points.downvote(req.params.name);
     res.json(points.getUser(req.params.name));
 });
 
-server.get('/adduser/:name', (req, res) => {
-    if (!limit(ip(req))) {
-        res.statusCode = 429;
-        return res.end();
-    }
-
+server.get('/adduser/:name', brute.prevent, (req, res) => {
     points.addUser(req.params.name);
     res.json(points.getUsers());
 });
@@ -53,30 +45,3 @@ server.listen(1716, () => console.log('Listening on port 1716'));
 setInterval(() => backup(), 60000);
 
 points.loadFromBackup();
-
-function ip(req: express.Request) {
-    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-}
-
-function limit(address: string): boolean {
-    // REQ_LIMIT requests per RESET_MS milliseconds
-    const REQ_LIMIT = 50;
-    const RESET_MS = 1000;
-
-    if (limitCache[address] === undefined) {
-        limitCache[address] = 0;
-        setTimeout(() => delete limitCache[address], RESET_MS);
-    }
-
-    limitCache[address]++;
-    const canRequest = limitCache[address] < REQ_LIMIT;
-
-    return canRequest;
-}
-
-function decrease(address: string): void {
-    if (limitCache[address] === undefined) return;
-    limitCache[address]--;
-    if (limitCache[address] > 0) return;
-    delete limitCache[address];
-}
