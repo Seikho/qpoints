ccc2693a3cf4b3c9ef01ed0b0c6a8707acc370a8
Carl Winkler <carl@winkler.id.au>
2018-03-03 23:54:23 +0800

    Move user data to data folder

--- src/backup.ts
+++ src/backup.ts
@@ -4,12 +4,12 @@ import { getUsers } from './points'
 export default function backup() {
   const users = getUsers()
   const json = JSON.stringify(getUsers())
-  fs.writeFileSync('users.json', json)
+  fs.writeFileSync('data/users.json', json)
 }
 
 export function readBackup() {
   try {
-    const json = fs.readFileSync('users.json').toString()
+    const json = fs.readFileSync('data/users.json').toString()
     const users = JSON.parse(json)
     return users
   } catch (ex) {
